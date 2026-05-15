c8f868d06eafba2e4cfc12455ee9ec810b9af352
d1yorkhud <d1yorkhud@gmail.com>
2026-05-16 00:55:05 +0900

    changing passwords

--- config/settings.py
+++ config/settings.py
@@ -127,3 +127,7 @@ LOGOUT_REDIRECT_URL = 'home'
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+
+#EMAIL BACKEND
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
