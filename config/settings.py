5c7e5ad0fb48b95b0e41ad1ae753cdf805806105
d1yorkhud <d1yorkhud@gmail.com>
2026-05-15 20:12:40 +0900

    fixing crispy_forms error

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms',
+    'crispy_bootstrap4',
     'accounts',
     'pages',
 ]
@@ -123,3 +124,6 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
