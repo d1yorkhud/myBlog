89c666b98c2029c83c8d24365a81cad3f696aba5
d1yorkhud <d1yorkhud@gmail.com>
2026-06-03 21:21:32 +0900

    csrf origins

--- config/settings.py
+++ config/settings.py
@@ -32,6 +32,9 @@ SECRET_KEY = env.str("SECRET_KEY")
 DEBUG = env.bool("DEBUG", default=False)
 
 ALLOWED_HOSTS = [".railway.app", "127.0.0.1"]
+CSRF_TRUSTED_ORIGINS = [
+    "https://web-production-83492.up.railway.app/",
+]
 
 
 # Application definition
